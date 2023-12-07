4076cfc3197001aca74f7e1e5e5d9c7900d8335c
KT2891 <t.k.6.2.1995@gmail.com>
2023-12-07 04:46:04 +0000

    create user admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   get '/about', to: "homes#about", as: "about"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #会員用
+  devise_for :users,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+  # 管理者用
+  # URL /admin/sign_in
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+
 end
