bb22e9ddf766972602bf9c167d95533bfdede5eb
KT2891 <t.k.6.2.1995@gmail.com>
2023-12-10 10:17:28 +0000

    sidebar create

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -1,20 +1,18 @@
-document.addEventListener('turbolinks:load', () => {
-  const loginForm = document.querySelector('#login-form');
-  if (loginForm) {
-    loginForm.addEventListener('ajax:error', (event) => {
-      const [data, status, xhr] = event.detail;
-      console.log(data);
-      const errorElement = document.querySelector('#login-error-messages');
-      if (errorElement) {
-        errorElement.innerHTML = '';
+/*global $*/
+$(document).on('turbolinks:load', function() {
+  const $loginForm = $('#login-form');
+  if ($loginForm.length) {
+    $loginForm.on('ajax:error', function(event, data, status, xhr) {
+      const $errorElement = $('#login-error-messages');
+      if ($errorElement.length) {
+        $errorElement.empty();
         if (data.errors) {
-          data.errors.forEach((error) => {
-            const errorItem = document.createElement('div');
-            errorItem.textContent = error;
-            errorElement.appendChild(errorItem);
+          data.errors.forEach(function(error) {
+            const $errorItem = $('<div>').text(error);
+            $errorElement.append($errorItem);
           });
         }
       }
     });
   }
-})
+});
