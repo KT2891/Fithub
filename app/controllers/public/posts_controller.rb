57a343ce69a4a267b38abac5c070765585dfb30d
KT2891 <t.k.6.2.1995@gmail.com>
2023-12-10 11:27:27 +0000

    posts 途中まで

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,7 +1,23 @@
 class Public::PostsController < ApplicationController
   def index
+    @posts = Post.all
+    @post = Post.new
   end
 
   def show
+  end
+
+  def create
+    if post = current_user.posts.create(post_params)
+      redirect_to posts_path
+    else
+      flash[:alert] = t("post-error-message")
+      render :index
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:body)
   end
 end
