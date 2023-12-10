da03fd431c62c93afe9f600949c3236abfdce91f
KT2891 <t.k.6.2.1995@gmail.com>
2023-12-10 07:13:01 +0000

    create login-form

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -3,6 +3,20 @@
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
+  def create
+  self.resource = warden.authenticate(auth_options)
+    if self.resource
+      set_flash_message!(:notice, :signed_in)
+      sign_in(resource_name, resource)
+      respond_with resource, location: after_sign_in_path_for(resource)
+    else
+      respond_to do |format|
+        format.json { render json: { errors: [t("log-in-error-message")] }, status: :unprocessable_entity }
+        format.html { render 'public/homes/top' }
+      end
+    end
+  end
+
   # GET /resource/sign_in
   # def new
   #   super
