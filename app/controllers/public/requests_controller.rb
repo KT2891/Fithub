118abf84685c184a76d5d63b836eefcc7c51d2be
KT2891 <t.k.6.2.1995@gmail.com>
2024-01-29 04:48:07 +0000

    リクエストの変数名変更

--- app/controllers/public/requests_controller.rb
+++ app/controllers/public/requests_controller.rb
@@ -2,8 +2,8 @@ class Public::RequestsController < ApplicationController
   before_action :authenticate_user!, except: :create
   
   def create
-    request = Request.new(request_params)
-    if request.save
+    makerequest = Request.new(request_params)
+    if makerequest.save
       redirect_to request.referer, notice: t("success-request")
     else
       redirect_to root_path, alert: t("error-request")
